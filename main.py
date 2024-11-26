1eff7880d790c2b5aa3284848b0d9bb9f773789b
kozzee <config@email.com>
2024-11-26 19:55:58 +0300

    Импортировал новые бибилиотеки и добавил функцию создания списка тем для статей

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import personal      #_____________________________________________________________________________________________Храню тут свои данные
+import personal, logging, os, asyncio, schedule, time      #_____________________________________________________________________________________________persional - Храню тут свои данные
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-import logging       #_____________________________________________________________________________________________Буду логировать все ошибки
-import asyncio
-import os            #_____________________________________________________________________________________________Прячу свои токены
+from gigachat import GigaChat
 
-API_TOKEN = os.getenv('API_TOKEN') #_______________________________________________________________________________Достаю токены из своей системы
+
+API_TOKEN = os.getenv('API_TOKEN') #_______________________________________________________________________________Достаю токены из своей локальной системы
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 # Токен вашего бота
 API_TOKEN = personal.BOT_TOKEN
-
 # Создание объектов Bot и Dispatcher
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher() 
+
+
+
+def create_article():
+    with GigaChat(credentials=API_TOKEN, verify_ssl_certs=False) as giga:
+                response = giga.chat(f'{personal.prompt_for_list}')
+    str_artcile = response.choices[0].message.content
+    list_article = str_artcile.split("|")
+    return list_article
+
+
+
+
 
 @dp.message(CommandStart())     #__________________________________________________________________________________Ответ на команду /start
 async def start_command(message: Message):
